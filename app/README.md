eaa3418c1cbffbdfc11cba053998b6e36533c04c
Kat-An <84103015+Kat-An@users.noreply.github.com>
2021-08-24 18:07:22 -0400

    Add files via upload
    Uploaded Hotdog Not Hotdog GP sample.

---
+++ app/README.md
@@ -0,0 +1,1 @@
+# PyTorch Classification Demo
---
+++ app/config.py
@@ -0,0 +1,15 @@
+"""
+This file contains all configurables of our app.
+
+@author: Weiqing Wang <Weiqing.Wang@ibm.com>, Kathy An <Kathy.An@ibm.com>
+"""
+
+
+config = {
+    "model_weight": "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/hotdog-not-hotdog/data"
+                    "/model_weight.pt",
+    # TODO: Change it!
+    "project_name": "Hotdog or Not Hotdog Detector",
+    # TODO: Change it!
+    "project_description": "Have you ever wondered whether or not an image had a hotdog in it? Wonder no further because the Hotdog Not Hotdog Detector can do just that!"
+}
